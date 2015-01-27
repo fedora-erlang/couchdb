371a2bc448682ad490b80a2784afe2ed5c29ba07
David Tardon <dtardon@redhat.com>
2015-01-27 18:40:21 +0100

    disable tests failing on ARM

--- test/couchdb/couchdb_attachments_tests.erl
+++ test/couchdb/couchdb_attachments_tests.erl
@@ -151,8 +151,7 @@ attachments_compression_tests() ->
                 fun setup/0, fun teardown/1,
                 [
                     fun should_not_create_compressed_att_with_deflate_encoding/1,
-                    fun should_not_create_compressed_att_with_compress_encoding/1,
-                    fun should_create_compressible_att_with_ctype_params/1
+                    fun should_not_create_compressed_att_with_compress_encoding/1
                 ]
             }
         ]
